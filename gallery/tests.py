9af36d94ac5e3d1718bb22bf794e2c233971f7c1
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-10-05 20:42:19 +0300

    Defines the save method test.Test fails.

--- gallery/tests.py
+++ gallery/tests.py
@@ -10,3 +10,8 @@ class LocationTestClass(TestCase):
 
     def test_isinstance(self):
         self.assertTrue(isinstance(self.California,Location))
+
+    def test_save_method(self):
+        self.California.save_method(self)
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
