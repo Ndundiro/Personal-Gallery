fa0cfb4f30f66e4f3b4b64ad111f7a390ea22ddb
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-10-05 20:37:42 +0300

    A dds a testcase for the Location class.

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Image,Category, Location
 
-# Create your tests here.
+# Create your tests h
+
+class LocationTestClass(TestCase):
+
+    def setUp(self):
+        self.California = Location(location = 'California')
+
+    def test_isinstance(self):
+        self.assertTrue(isinstance(self.California,Location))
