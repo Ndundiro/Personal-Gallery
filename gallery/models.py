086b853b4e84ebfc50487741da797f03ef1fcc8c
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-10-05 06:11:57 +0300

    Adds the Class location and its class variables to describe the location where a photo was taken.

--- gallery/models.py
+++ gallery/models.py
@@ -1,8 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    location = models.CharField(max_length =12)
+
+
+    def __str__(self):
+        return self.location
+
+
+
 class Image(models.Model):
     description = models.CharField(max_length =30)
+    location = models.ForeignKey(Location)
+    Category = models.ManyToManyField(Category)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
 
     def __str__(self):
         return self.description
+
