9216757ba7c248c9f44a270320714bbffc75a979
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-10-07 00:35:56 +0300

    Adds the search results function that gets search results.

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Image, Location
 
 # Create your views here.
 def index(request):
-    return HttpResponse("The gallery app")
+    images = Image.objects.all()
+    location = Location.objects.all()
+
+    return render(request, 'index.html', {'images':images})
+
+
+
+
+
+
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, '/search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
