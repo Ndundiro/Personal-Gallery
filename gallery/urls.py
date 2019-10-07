47873a9b8588d4aee439cb0eafe3477349aba05f
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-10-07 08:38:28 +0300

    adds the search and images url

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,9 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import  url
 from . import views
 
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^search/', views.search_results, name='search_results')
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^image/(\d+)', views.image, name='image'),
    
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
